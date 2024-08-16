328f58487c338bc394685e73e8cbcc8fdff98931
Grant Reighard <grant.reighard@gmail.com>
2024-08-15 20:01:36 -0400

    Adds working Iceberg strat

---
+++ strategies/QuantVueIceberg.cs
@@ -0,0 +1,160 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+//This namespace holds Strategies in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class QuantVueIceberg : Strategy
+	{
+		
+		private Moneyball Moneyball1;
+		private Qcloud Qcloud1;
+		private Qwave Qwave1;
+		private MACD MACD1;
+		private bool isBreakevenSet = false;
+		
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"Enter the description for your new custom Strategy here.";
+				Name										= "QuantVueIceberg";
+				Calculate									= Calculate.OnBarClose;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= true;
+				ExitOnSessionCloseSeconds					= 30;
+				IsFillLimitOnTouch							= false;
+				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.ImmediatelySubmit;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 30;
+				// Disable this property for performance gains in Strategy Analyzer optimizations
+				// See the Help Guide for additional information
+				IsInstantiatedOnEachOptimizationIteration	= true;
+				
+				TP = 300;
+				SL = 100;
+				DQ = 10;
+				BreakevenProfit = 5;
+				FastPeriod = 12;  
+                SlowPeriod  = 26;  
+                SignalPeriod  = 9; 
+			}
+			else if (State == State.Configure)
+			{
+				DefaultQuantity = DQ;
+			}
+			else if (State == State.DataLoaded)
+			{				
+				Moneyball1 = Moneyball(Close, Brushes.RoyalBlue, Brushes.Blue, 15, 10, true, 0.35, -0.35, 0.1, MoneyballMode.M);
+				Qcloud1 = Qcloud(Close, Brushes.Red, Brushes.Green, 19, 29, 49, 59, 69, 99);
+				Qwave1 = Qwave(Close, 55, 256, 1.5, 0.1, 9, false, false, Brushes.Transparent);
+				MACD1 = MACD(Close, FastPeriod, SlowPeriod, SignalPeriod);
+			}
+		}
+
+		protected override void OnBarUpdate()
+		{			
+			
+			if (BarsInProgress != 0 || CurrentBar < BarsRequiredToTrade)
+				return;
+			
+
+			if (CrossAbove(Moneyball1.VBar, 0.35, 5) && CrossAbove(Qcloud1.V1, Qcloud1.V6,51) && CrossAbove(Qwave1.K1, Qwave1.VHigh, 5) && CrossAbove(MACD1.Avg, MACD1.Default, 5)  && Close[0] > SMA(Close, 21)[0])
+			{
+				EnterLong(Convert.ToInt32(DefaultQuantity), "GoLong");
+				SetStopLoss("GoLong", CalculationMode.Currency, SL, false);
+				isBreakevenSet = false;
+			}
+			
+			if (CrossBelow(Moneyball1.VBar, -0.35, 5) && CrossBelow(Qcloud1.V1, Qcloud1.V6, 5) && CrossBelow(Qwave1.VLow, Qwave1.K1, 5) && CrossBelow(MACD1.Avg, MACD1.Default, 5) && Close[0] < SMA(Close, 21)[0])
+			{
+				EnterShort(Convert.ToInt32(DefaultQuantity), "GoShort");
+				SetStopLoss("GoShort", CalculationMode.Currency, SL, false);
+				isBreakevenSet = false;
+			}
+			
+			if (BreakevenProfit > 0)
+			{
+				if (Position.MarketPosition != MarketPosition.Flat && (Position.GetUnrealizedProfitLoss(PerformanceUnit.Currency, Close[0]) >= BreakevenProfit) && !isBreakevenSet)
+				{
+					SetStopLoss("GoLong", CalculationMode.Price, Position.AveragePrice + (BreakevenProfit / 2), true);
+					SetStopLoss("GoShort", CalculationMode.Price, Position.AveragePrice + (BreakevenProfit / 2), true);
+					isBreakevenSet = true;
+				}
+			}
+			
+		}
+		
+		#region Properties
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="TP", Order=1, GroupName="Parameters")]
+		public int TP
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="SL", Order=2, GroupName="Parameters")]
+		public int SL
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="DQ", Order=3, GroupName="Parameters")]
+		public int DQ
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Range(0, int.MaxValue)]
+		[Display(Name="Breakeven Profit", Order=4, GroupName="Parameters")]
+		public int BreakevenProfit
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name="Fast Period", Order=5, GroupName="Parameters")]
+        public int FastPeriod 
+		{ get; set; }
+
+		[NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name="Slow Period", Order=6, GroupName="Parameters")]
+        public int SlowPeriod 
+		{ get; set; }
+
+		[NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name="Signal Period", Order=7, GroupName="Parameters")]
+        public int SignalPeriod 
+		{ get; set; }
+        #endregion
+	}
+}
