5b5ec3966fa606a063459513075fb0db98e5db41
Austin Rhoads <oostinr@gmail.com>
2024-08-20 22:10:40 -0400

    Add SL mode selection, Indicator Param Pass
    SL can be changed between BE Only and Step SL
    
    Step SL uses a user input tick amount to step the SL up after the SL moves to BE.
    
    Adds parameters to pass on to Moneyball indicator.

--- strategies/QuantVueIceberg.cs
+++ strategies/QuantVueIceberg.cs
@@ -37,7 +37,7 @@ namespace NinjaTrader.NinjaScript.Strategies
 		private DateTime 								startTime 			= DateTime.Parse("11:00:00", System.Globalization.CultureInfo.InvariantCulture);
 		private DateTime		 						endTime 			= DateTime.Parse("13:00:00", System.Globalization.CultureInfo.InvariantCulture);
 		private	CustomEnumNamespaceIce.StopMode			StopModeSelect		= CustomEnumNamespaceIce.StopMode.BEOnly;
-		private int										tickCount			= 0;
+		private int										tickCount			= 1;
 		
 		
 		protected override void OnStateChange()
@@ -72,7 +72,13 @@ namespace NinjaTrader.NinjaScript.Strategies
 				BreakevenProfit = 0;
 				FastPeriod = 12;  
                 SlowPeriod  = 26;  
-                SignalPeriod  = 9; 
+                SignalPeriod  = 9;
+				mb_Nb_bars = 15;
+				mb_period = 10;
+				mb_zero = true;
+				mb_uThreshold = 0.35;
+				mb_lThreshold = -0.35;
+				mb_Sensitivity = 0.1;
 			}
 			else if (State == State.Configure)
 			{
@@ -80,11 +86,11 @@ namespace NinjaTrader.NinjaScript.Strategies
 			}
 			else if (State == State.DataLoaded)
 			{				
-				Moneyball1 = Moneyball(Close, Brushes.RoyalBlue, Brushes.Blue, 15, 10, true, 0.35, -0.35, 0.1, MoneyballMode.M, false);
+				Moneyball1 = Moneyball(Close, Brushes.RoyalBlue, Brushes.Blue, mb_Nb_bars, mb_period, mb_zero, mb_uThreshold, mb_lThreshold, mb_Sensitivity, MoneyballMode.M, false);
 				Qcloud1 = Qcloud(Close, Brushes.Red, Brushes.Green, 19, 29, 49, 59, 69, 99, false);
 				Qwave1 = Qwave(Close, 55, 256, 1.5, 0.1, 9, false, false, Brushes.Transparent, false);
 				MACD1 = MACD(Close, FastPeriod, SlowPeriod, SignalPeriod);
-				AddChartIndicator(Moneyball(Close, Brushes.RoyalBlue, Brushes.Blue, 15, 10, true, 0.35, -0.35, 0.1, MoneyballMode.M, false));
+				AddChartIndicator(Moneyball(Close, Brushes.RoyalBlue, Brushes.Blue, mb_Nb_bars, mb_period, true, mb_uThreshold, mb_lThreshold, mb_Sensitivity, MoneyballMode.M, false));
 				AddChartIndicator(Qcloud(Close, Brushes.Red, Brushes.Green, 19, 29, 49, 59, 69, 99, false));
 				AddChartIndicator(Qwave(Close, 55, 256, 1.5, 0.1, 9, false, false, Brushes.Transparent, false));
 				AddChartIndicator(MACD(Close, FastPeriod, SlowPeriod, SignalPeriod));
@@ -101,7 +107,7 @@ namespace NinjaTrader.NinjaScript.Strategies
 			if ((ToTime(Time[0]) >= ToTime(startTime) && ToTime(Time[0]) <= ToTime(endTime)) || TimeModeSelect == CustomEnumNamespaceIce.TimeMode.Unrestricted)
 			{
 
-				if (CrossAbove(Moneyball1.VBar, 0.35, LookbackPeriod) && CrossAbove(Qcloud1.V1, Qcloud1.V6, LookbackPeriod) && CrossAbove(Qwave1.K1, Qwave1.VHigh, LookbackPeriod) && CrossAbove(MACD1.Default, MACD1.Avg, LookbackPeriod)  && Close[0] > SMA(Close, 21)[0])
+				if (CrossAbove(Moneyball1.VBar, mb_uThreshold, LookbackPeriod) && CrossAbove(Qcloud1.V1, Qcloud1.V6, LookbackPeriod) && CrossAbove(Qwave1.K1, Qwave1.VHigh, LookbackPeriod) && CrossAbove(MACD1.Default, MACD1.Avg, LookbackPeriod)  && Close[0] > SMA(Close, 21)[0])
 				{
 					EnterLong(Convert.ToInt32(DefaultQuantity), "GoLong");
 					SetStopLoss("GoLong", CalculationMode.Currency, SL, false);
@@ -109,7 +115,7 @@ namespace NinjaTrader.NinjaScript.Strategies
 					isBreakevenSet = false;
 				}
 			
-				if (CrossBelow(Moneyball1.VBar, -0.35, LookbackPeriod) && CrossBelow(Qcloud1.V1, Qcloud1.V6, LookbackPeriod) && CrossBelow(Qwave1.K1, Qwave1.VLow, LookbackPeriod) && CrossBelow(MACD1.Default, MACD1.Avg, LookbackPeriod) && Close[0] < SMA(Close, 21)[0])
+				if (CrossBelow(Moneyball1.VBar, mb_lThreshold, LookbackPeriod) && CrossBelow(Qcloud1.V1, Qcloud1.V6, LookbackPeriod) && CrossBelow(Qwave1.K1, Qwave1.VLow, LookbackPeriod) && CrossBelow(MACD1.Default, MACD1.Avg, LookbackPeriod) && Close[0] < SMA(Close, 21)[0])
 				{
 					EnterShort(Convert.ToInt32(DefaultQuantity), "GoShort");
 					SetStopLoss("GoShort", CalculationMode.Currency, SL, false);
@@ -128,18 +134,20 @@ namespace NinjaTrader.NinjaScript.Strategies
 					{
 						SetStopLoss("GoLong", CalculationMode.Price, Position.AveragePrice, false);
 						isBreakevenSet = true;
+						tickCount = 1;
 					}
 					else if (Position.MarketPosition == MarketPosition.Short)
 					{
 						SetStopLoss("GoShort", CalculationMode.Price, Position.AveragePrice, false);
 						isBreakevenSet = true;
+						tickCount = 1;
 					}
 				}
 			}
 			
 			// Step Stop Loss Mode //
 			
-			if (BreakevenProfit > 0 && StopModeSelect == CustomEnumNamespaceIce.StopMode.StepSL && isBreakevenSet == true)
+			if (StopModeSelect == CustomEnumNamespaceIce.StopMode.StepSL && isBreakevenSet == true)
 			{
 				/*Commenting out for testing
 				if (Position.MarketPosition == MarketPosition.Long && Close[0] >= Position.AveragePrice + slStepSize * TickSize && isBreakevenSet == false)
@@ -152,18 +160,18 @@ namespace NinjaTrader.NinjaScript.Strategies
 				
 				if (Position.MarketPosition == MarketPosition.Long)
 				{
-					if (Close[0] > Position.AveragePrice + (slStepSize +tickCount) * TickSize) // adjust higher each time by tickCount
+					if (Close[0] > Position.AveragePrice + ((slStepSize + tickCount) * TickSize)) // adjust higher each time by tickCount
 					{
-						SetStopLoss("GoLong", CalculationMode.Price, Position.AveragePrice+(tickCount * TickSize), false);
+						SetStopLoss("GoLong", CalculationMode.Price, Position.AveragePrice + (tickCount * TickSize), false);
 						tickCount ++; // increment to next tick
 					}
 				}
 
 				if (Position.MarketPosition == MarketPosition.Short)
 				{
-					if (Close[0] > Position.AveragePrice - (slStepSize +tickCount) * TickSize) // adjust higher each time by tickCount
+					if (Close[0] < Position.AveragePrice - (slStepSize + tickCount) * TickSize) // adjust higher each time by tickCount
 					{
-						SetStopLoss("GoShort", CalculationMode.Price, Position.AveragePrice-(tickCount * TickSize), false);
+						SetStopLoss("GoShort", CalculationMode.Price, Position.AveragePrice - (tickCount * TickSize), false);
 						tickCount ++; // increment to next tick
 					}
 				}
@@ -202,49 +210,49 @@ namespace NinjaTrader.NinjaScript.Strategies
 		
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name="TP (Currency)", Order=1, GroupName="Parameters")]
+		[Display(Name="TP (Currency)", Order=1, GroupName="Trade Parameters")]
 		public int TP
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name="SL (Currency)", Order=2, GroupName="Parameters")]
+		[Display(Name="SL (Currency)", Order=2, GroupName="Trade Parameters")]
 		public int SL
 		{ get; set; }
 
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name="Default Quantity (Contracts)", Order=3, GroupName="Parameters")]
+		[Display(Name="Default Quantity (Contracts)", Order=3, GroupName="Trade Parameters")]
 		public int DQ
 		{ get; set; }
 		
 		[NinjaScriptProperty]
 		[Range(0, int.MaxValue)]
-		[Display(Name="Breakeven Profit (Currency)", Order=4, GroupName="Parameters")]
+		[Display(Name="Breakeven Profit (Currency)", Order=4, GroupName="Trade Parameters")]
 		public int BreakevenProfit
 		{ get; set; }
 		
 		[NinjaScriptProperty]
         [Range(1, int.MaxValue)]
-        [Display(Name="Lookback Period", Order=5, GroupName="Parameters")]
+        [Display(Name="Lookback Period", Order=5, GroupName="Trade Parameters")]
         public int LookbackPeriod 
 		{ get; set; }
-		
+				
 		[NinjaScriptProperty]
         [Range(1, int.MaxValue)]
-        [Display(Name="Fast Period", Order=6, GroupName="Parameters")]
+        [Display(Name="Fast Period", Order=6, GroupName="Trade Parameters")]
         public int FastPeriod 
 		{ get; set; }
 
 		[NinjaScriptProperty]
         [Range(1, int.MaxValue)]
-        [Display(Name="Slow Period", Order=7, GroupName="Parameters")]
+        [Display(Name="Slow Period", Order=7, GroupName="Trade Parameters")]
         public int SlowPeriod 
 		{ get; set; }
 
 		[NinjaScriptProperty]
         [Range(1, int.MaxValue)]
-        [Display(Name="Signal Period", Order=8, GroupName="Parameters")]
+        [Display(Name="Signal Period", Order=8, GroupName="Trade Parameters")]
         public int SignalPeriod 
 		{ get; set; }
 		
@@ -261,6 +269,42 @@ namespace NinjaTrader.NinjaScript.Strategies
 		[Display(ResourceType = typeof(Custom.Resource), Name = "Stoploss Step Distance (Ticks)", GroupName = "SL Parameters", Order = 1)]
 		public int slStepSize
 		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="Number of bars between signals", Order=0, GroupName="Moneyball Parameters")]
+		public int mb_Nb_bars
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Range(1, int.MaxValue)]
+		[Display(Name="Period", Order=1, GroupName="Moneyball Parameters")]
+		public int mb_period
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Display(Name="All Zero", Order=2, GroupName="Moneyball Parameters")]
+		public bool mb_zero
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Range(.001, 1.0)]
+		[Display(Name="Upper Threshold", Order=4, GroupName="Moneyball Parameters")]
+		public double mb_uThreshold
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Range(-1.0, -.001)]
+		[Display(Name="Lower Threshold", Order=5, GroupName="Moneyball Parameters")]
+		public double mb_lThreshold
+		{ get; set; }
+		
+		[NinjaScriptProperty]
+		[Range(0.001, double.MaxValue)]
+		[Display(Name="Sensitivity", Order=6, GroupName="Moneyball Parameters")]
+		public double mb_Sensitivity
+		{ get; set; }
+		
         #endregion
 	}
 }
